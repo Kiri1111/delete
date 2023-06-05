ba473abe97f99dc5819a9af821ca7de7e60209aa
Kiri1111 <92_medved@mail.ru>
2023-06-05 20:18:16 +0300

    delete: clear project

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,10 @@
-import React, {useState} from 'react';
-import './App.css';
-import {Comments} from "./components/comments";
-import SuperButton from "./components/SuperButton";
-import {useDispatch, useSelector} from "react-redux";
-import {AppRootStateType, useAppDispatch} from "./store/store";
-import {
-    addAllCommentsTC,
-    delCommentTC,
-    deleteOneCommentAC,
-    InitStateType,
-    updateTitleTC
-} from "./reducers/CommentsReducer";
-import {ThunkDispatch} from "redux-thunk";
-import {AnyAction} from "redux";
+import React from 'react';
 
-export type CommentType = {
-    postId: number
-    id: number
-    name: string
-    email: string
-    body: string
-    isLoading: boolean
-}
-
-function App() {
-    const state = useSelector<AppRootStateType, InitStateType>(st => st.comments)
-    const dispatch = useAppDispatch()
-    const [isLoading, setIsLoading] = useState(false)
-
-    const getCommentsHandler = () => {
-        setIsLoading(true)
-        dispatch(addAllCommentsTC())
-        setIsLoading(false)
-    }
-
-    const deleteAllCommentsHandler = () => {
-        setIsLoading(true)
-        // getComments.delAllComments()
-        // .then(result => setComments(result.data))
-        setIsLoading(false)
-    }
-
-    const deleteOneCommentHandler = (idComment: number) => {
-        dispatch(delCommentTC(idComment))
-    }
-    const updateCommentTitle = () => {
-        dispatch(updateTitleTC('New title'))
-    }
-
-    return <div>
+export const App = () => {
+    return (
         <div>
-            <SuperButton isLoading={isLoading} title={'add comments'} callBack={getCommentsHandler}/>
-            <SuperButton isLoading={isLoading} title={'del comments'} callBack={deleteAllCommentsHandler}/>
-            <Comments updateCommentTitle={updateCommentTitle} delComment={deleteOneCommentHandler} isLoading={isLoading}
-                      comments={state}/>
+            hi
         </div>
-    </div>
-}
+    );
+};
 
-export default App;
